2ddd6c13bba61f66532742adc2cf4b42976f8d55
DallonF <dallon.feldner@gmail.com>
2017-12-05 07:47:12 -0600

    Day 5 Part Two

--- day5.ts
+++ day5.ts
@@ -14,6 +14,19 @@ const jumpEscape1 = (offsets: number[]) => {
   return steps;
 };
 
+const jumpEscape2 = (offsets: number[]) => {
+  const mutableOffsets = [...offsets];
+  let cursor = 0,
+    steps = 0;
+  while (cursor >= 0 && cursor < offsets.length) {
+    const offset = mutableOffsets[cursor];
+    mutableOffsets[cursor] += offset >= 3 ? -1 : 1;
+    steps += 1;
+    cursor += offset;
+  }
+  return steps;
+};
+
 const puzzleInput = readLines('./day5input.txt').map(line =>
   parseInt(line, 10)
 );
@@ -21,3 +34,7 @@ const puzzleInput = readLines('./day5input.txt').map(line =>
 console.log('Part One');
 simpleTest(jumpEscape1, [0, 3, 0, 1, -3], 5);
 test('Part One Answer', equalResult(jumpEscape1(puzzleInput), 391540));
+
+console.log('Part Two');
+simpleTest(jumpEscape2, [0, 3, 0, 1, -3], 10);
+test('Part Two Answer', equalResult(jumpEscape2(puzzleInput), 30513679));
