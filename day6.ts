c9c7190e81743b0444559ee73ac79e3eaa12fbec
DallonF <dallon.feldner@gmail.com>
2017-12-06 08:22:59 -0600

    Day 6 Part Two

--- day6.ts
+++ day6.ts
@@ -1,15 +1,14 @@
 import test, { equalResult, simpleTest } from './test';
-import * as _ from 'lodash';
 
 const hashState = (banks: number[]) => banks.map(n => n.toString()).join('-');
 
-const redistribute1 = (banks: number[]) => {
-  const stateMemory = new Set<string>();
+const redistribute = (banks: number[]) => {
+  const stateMemory = new Map<string, { index: number }>();
   let steps = 0,
     currentState = banks;
 
   while (!stateMemory.has(hashState(currentState))) {
-    stateMemory.add(hashState(currentState));
+    stateMemory.set(hashState(currentState), { index: steps });
 
     const {
       index: maxIndex,
@@ -40,11 +39,24 @@ const redistribute1 = (banks: number[]) => {
     steps += 1;
     currentState = newState;
   }
-  return steps;
+  const cycleStart = stateMemory.get(hashState(currentState))!;
+  return { steps, result: currentState, cycleLength: steps - cycleStart.index };
+};
+
+const stepsToLoop = (banks: number[]) => {
+  return redistribute(banks).steps;
+};
+
+const stepsBetweenLoops = (banks: number[]) => {
+  return redistribute(banks).cycleLength;
 };
 
 const PUZZLE_INPUT = [10, 3, 15, 10, 5, 15, 5, 15, 9, 2, 5, 8, 5, 2, 3, 6];
 
 console.log('Part One');
-simpleTest(redistribute1, [0, 2, 7, 0], 5);
-test('Part One answer', equalResult(redistribute1(PUZZLE_INPUT), 14029));
+simpleTest(stepsToLoop, [0, 2, 7, 0], 5);
+test('Part One answer', equalResult(stepsToLoop(PUZZLE_INPUT), 14029));
+
+console.log('Part Two');
+simpleTest(stepsBetweenLoops, [0, 2, 7, 0], 4);
+test('Part Two answer', equalResult(stepsBetweenLoops(PUZZLE_INPUT), 2765));
