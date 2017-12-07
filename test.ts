c1b7f3eccac56cec710c92e84887638178b4b43c
DallonF <dallon.feldner@gmail.com>
2017-12-07 06:40:22 -0600

    Day 07 Parsing logic

--- test.ts
+++ test.ts
@@ -1,7 +1,12 @@
 import chalk from 'chalk';
+import * as _ from 'lodash';
 
 interface TestFailure {
   failure: string;
+}
+
+interface EqualOptions {
+  deepEqual?: boolean;
 }
 
 const test = (testname: string, result: boolean | TestFailure) => {
@@ -14,20 +19,30 @@ const test = (testname: string, result: boolean | TestFailure) => {
   }
 };
 
-export const equalResult = <T>(actual: T, expected: T) =>
-  expected === actual ? true : { failure: `Got ${actual}` };
+export const equalResult = <T>(
+  actual: T,
+  expected: T,
+  options: EqualOptions = {}
+) =>
+  (options.deepEqual ? _.isEqual(expected, actual) : expected === actual)
+    ? true
+    : { failure: `Got ${stringRepresentation(actual)}` };
+
+const stringRepresentation = (value: any) =>
+  typeof value === 'string' ? value : JSON.stringify(value);
 
 export const simpleTest = <TInput, TResult>(
   func: (input: TInput) => TResult,
   input: TInput,
   expectedOutput: TResult,
-  prefix?: string
+  prefix?: string,
+  options?: EqualOptions
 ) =>
   test(
-    `${prefix ? prefix + ' ' : ''}input:${
-      typeof input === 'string' ? input : JSON.stringify(input)
-    } = ${expectedOutput}`,
-    equalResult(func(input), expectedOutput)
+    `${prefix ? prefix + ' ' : ''}input:${stringRepresentation(
+      input
+    )} = ${stringRepresentation(expectedOutput)}`,
+    equalResult(func(input), expectedOutput, options)
   );
 
 export default test;
