bbb7d97960c818d433180734b7ebdd5913c0e2e3
DallonF <dallon.feldner@gmail.com>
2017-12-09 10:37:35 -0600

    Day 9 Part One

--- day09.ts
+++ day09.ts
@@ -1,9 +1,18 @@
+import * as fs from 'fs';
 import test, { equalResult, simpleTest } from './test';
-import { debug } from 'util';
 
 const scoreStream = (input: string) => {
   try {
-    return parseGroup(input, { parentScore: 0, debugIndex: 0 }).score;
+    const trimmed = input.trim();
+    const result = parseGroup(input, { parentScore: 0, debugIndex: 0 });
+    if (result.remainingInput) {
+      throw new Error(
+        `${result.debugIndex}: Expected end of stream, got "${
+          result.remainingInput
+        }"`
+      );
+    }
+    return result.score;
   } catch (err) {
     throw new Error(`Error parsing "${input}": ${err.message}`);
   }
@@ -54,6 +63,17 @@ const parseGroupBody = (
         debugIndex: groupResult.debugIndex,
       });
     }
+    case '<': {
+      // garbage
+      const garbageResult = parseGarbage(input, {
+        debugIndex: context.debugIndex,
+      });
+      // keep parsing
+      return parseGroupBody(garbageResult.remainingInput, {
+        ...context,
+        debugIndex: garbageResult.debugIndex,
+      });
+    }
     case ',':
       // ignore
       return parseGroupBody(input.slice(1), {
@@ -62,10 +82,49 @@ const parseGroupBody = (
       });
     default:
       throw new Error(
-        `${context.debugIndex}: Unrecognized character "${char}"`
+        `${context.debugIndex}: Unrecognized group character "${char}"`
       );
   }
 };
+
+const parseGarbage = (input: string, context: { debugIndex: number }) => {
+  if (input.charAt(0) !== '<') {
+    throw new Error(
+      `${
+        context.debugIndex
+      }: Expected < at beginning of garbage, instead got ${input.charAt(0)}`
+    );
+  }
+  return parseGarbageBody(input.slice(1), {
+    debugIndex: context.debugIndex + 1,
+  });
+};
+
+const parseGarbageBody = (
+  input: string,
+  context: { debugIndex: number }
+): { remainingInput: string; debugIndex: number } => {
+  const char = input.charAt(0);
+  switch (char) {
+    case '>':
+      return {
+        remainingInput: input.slice(1),
+        debugIndex: context.debugIndex + 1,
+      };
+    case '!':
+      // skip the next character
+      return parseGarbageBody(input.slice(2), {
+        debugIndex: context.debugIndex + 2,
+      });
+    default:
+      // ignore everything inside garbage
+      return parseGarbageBody(input.slice(1), {
+        debugIndex: context.debugIndex + 1,
+      });
+  }
+};
+
+const PUZZLE_INPUT = fs.readFileSync('./day09input.txt', 'utf-8');
 
 console.log('Part One');
 simpleTest(scoreStream, '{}', 1);
@@ -76,3 +135,4 @@ simpleTest(scoreStream, '{<a>,<a>,<a>,<a>}', 1);
 simpleTest(scoreStream, '{{<ab>},{<ab>},{<ab>},{<ab>}}', 9);
 simpleTest(scoreStream, '{{<!!>},{<!!>},{<!!>},{<!!>}}', 9);
 simpleTest(scoreStream, '{{<a!>},{<a!>},{<a!>},{<ab>}}', 3);
+test('Part One answer', equalResult(scoreStream(PUZZLE_INPUT), 10616));
