fce1760978f4d78b088f02876e16e0c7c243a2db
DallonF <dallon.feldner@gmail.com>
2017-12-13 08:39:48 -0600

    Day 12 Part Two

--- day12.ts
+++ day12.ts
@@ -17,12 +17,32 @@ const parseLine = (input: string): Program => {
 };
 
 const crawlZeroGroup = (programs: Program[]) => {
-  const programMap = new Map<number, Program>(
-    programs.map(p => [p.id, p] as [number, Program])
-  );
-  const id = 0;
-  const group = new Set<number>([id]);
-  const crawlQueue = [id];
+  return crawlGroup(makeProgramMap(programs), 0).size;
+};
+
+const countGroups = (programs: Program[]) => {
+  const programMap = makeProgramMap(programs);
+  let groups = 0;
+  const programsAccountedFor = new Set<number>();
+  programs.forEach(p => {
+    if (!programsAccountedFor.has(p.id)) {
+      groups++;
+      const newGroup = crawlGroup(programMap, p.id);
+      newGroup.forEach(p => programsAccountedFor.add(p));
+    }
+  });
+  return groups;
+};
+
+const makeProgramMap = (programs: Program[]): Map<number, Program> =>
+  new Map<number, Program>(programs.map(p => [p.id, p] as [number, Program]));
+
+const crawlGroup = (
+  programMap: Map<number, Program>,
+  startingId: number
+): Set<number> => {
+  const group = new Set<number>([startingId]);
+  const crawlQueue = [startingId];
   while (crawlQueue.length) {
     const crawlId = crawlQueue.shift()!;
     const program = programMap.get(crawlId);
@@ -34,7 +54,7 @@ const crawlZeroGroup = (programs: Program[]) => {
       }
     });
   }
-  return group.size;
+  return group;
 };
 
 const EXAMPLE_INPUT = `
@@ -57,3 +77,6 @@ const PUZZLE_INPUT = readLines('./day12input.txt')
 console.log('Part One');
 test('crawlZeroGroup, expect 6', equalResult(crawlZeroGroup(EXAMPLE_INPUT), 6));
 test('Part One answer', equalResult(crawlZeroGroup(PUZZLE_INPUT), 113));
+console.log('Part Two');
+test('countGroups, expect 2', equalResult(countGroups(EXAMPLE_INPUT), 2));
+test('Part Two answer', equalResult(countGroups(PUZZLE_INPUT), 202));
