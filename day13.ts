1f6716097b9c374d980d7c54585b27e5d944407f
DallonF <dallon.feldner@gmail.com>
2017-12-13 20:18:31 -0600

    Day 13 Part Two... except it doesn't work yet

--- day13.ts
+++ day13.ts
@@ -1,4 +1,5 @@
 import test, { equalResult } from './test';
+import * as _ from 'lodash';
 import { OS_EOL, readLines } from './util';
 
 interface LayerDefinition {
@@ -19,11 +20,18 @@ const parseLine = (input: string): LayerDefinition => {
   return { depth: parseInt(match[1], 10), range: parseInt(match[2], 10) };
 };
 
-const getSeverityOfTrip = (layerDefinitions: LayerDefinition[]) => {
+const makeLayerStateArray = (
+  layerDefinitions: LayerDefinition[]
+): LayerStateArray => {
   const layers: LayerStateArray = [];
   layerDefinitions.forEach(ld => {
     layers[ld.depth] = { ...ld, scannerPosition: 0, direction: 1 };
   });
+  return layers;
+};
+
+const getSeverityOfTrip = (layerDefinitions: LayerDefinition[]) => {
+  const layers = makeLayerStateArray(layerDefinitions);
 
   const caughtInLayers = [];
   for (let i = 0; i < layers.length; i++) {
@@ -44,6 +52,40 @@ const getSeverityOfTrip = (layerDefinitions: LayerDefinition[]) => {
       return layer.depth * layer.range;
     })
     .reduce((a, b) => a + b);
+};
+
+interface PacketState {
+  delay: number;
+  position: number;
+}
+const calculateSafePassage = (layerDefinitions: LayerDefinition[]) => {
+  const layers = makeLayerStateArray(layerDefinitions);
+
+  let packetsInFlight: PacketState[] = [];
+  for (let i = 0; i < 1000; i++) {
+    // make a new packet
+    const newPacket: PacketState = {
+      delay: i,
+      position: 0,
+    };
+    packetsInFlight.push(newPacket);
+    // check if any packets have been caught
+    packetsInFlight = packetsInFlight.filter(p => {
+      const layer = layers[p.position];
+      return !(layer && layer.scannerPosition === 0);
+    });
+    // check if any packets have made it to the end
+    const winner = packetsInFlight.find(p => p.position >= layers.length - 1);
+    if (winner) {
+      return winner.delay;
+    }
+
+    updateScanners(layers);
+
+    // advance all packets
+    packetsInFlight.forEach(p => (p.position += 1));
+  }
+  throw new Error('Exited after high number of iterations without solution');
 };
 
 const updateScanners = (layers: LayerStateArray) => {
@@ -77,3 +119,7 @@ const PUZZLE_INPUT = readLines('./day13input.txt').map(parseLine);
 console.log('Part One');
 test('Severity', equalResult(getSeverityOfTrip(EXAMPLE_INPUT), 24));
 test('Part One Answer', equalResult(getSeverityOfTrip(PUZZLE_INPUT), 1728));
+
+console.log('Part Two');
+test('Safe Passage', equalResult(calculateSafePassage(EXAMPLE_INPUT), 10));
+test('Part Two Answer', equalResult(calculateSafePassage(PUZZLE_INPUT), 0));
