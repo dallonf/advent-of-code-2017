70a415026e270ff3ffda8eb47137160850b1cf83
DallonF <dallon.feldner@gmail.com>
2017-12-14 07:44:05 -0600

    Day 14 Part Two

--- day14.ts
+++ day14.ts
@@ -29,17 +29,63 @@ const createGrid = (key: string): Grid =>
     .map(binary => [...binary].map(bit => bit === '1'));
 
 const gridEntries = function*(input: Grid) {
-  for (let x = 0; x < input.length; x++) {
-    const column = input[x];
-    for (let y = 0; y < column.length; y++) {
-      const bit = column[y];
-      yield { x, y, bit };
+  for (let y = 0; y < input.length; y++) {
+    const row = input[y];
+    for (let x = 0; x < row.length; x++) {
+      const bit = row[x];
+      yield { y, x, bit };
     }
   }
 };
 
 const usedSquares = (key: string) =>
   [...gridEntries(createGrid(key))].filter(square => square.bit).length;
+
+const squareKey = ({ x, y }: { x: number; y: number }) => `${x},${y}`;
+const countRegions = (key: string) => {
+  const grid = createGrid(key);
+  const accountedForSquares = new Set<string>();
+
+  const groups = [];
+  for (const square of gridEntries(grid)) {
+    if (accountedForSquares.has(squareKey(square))) {
+      // This square is already part of a group, or is blank
+      continue;
+    } else if (square.bit) {
+      // Find its group
+      let groupSize = 0;
+      const groupCrawlQueue: { x: number; y: number }[] = [];
+      groupCrawlQueue.push(square);
+      while (groupCrawlQueue.length) {
+        const currentSquare = groupCrawlQueue.shift()!;
+        if (
+          currentSquare.x < 0 ||
+          currentSquare.x >= LENGTH ||
+          currentSquare.y < 0 ||
+          currentSquare.y >= LENGTH
+        ) {
+          // square is out of bounds
+          continue;
+        }
+        if (accountedForSquares.has(squareKey(currentSquare))) {
+          // Don't check this square, we already know where it belongs
+          continue;
+        }
+        if (grid[currentSquare.y][currentSquare.x]) {
+          groupSize += 1;
+          // add adjacent squares to queue
+          groupCrawlQueue.push({ x: currentSquare.x - 1, y: currentSquare.y });
+          groupCrawlQueue.push({ x: currentSquare.x + 1, y: currentSquare.y });
+          groupCrawlQueue.push({ x: currentSquare.x, y: currentSquare.y - 1 });
+          groupCrawlQueue.push({ x: currentSquare.x, y: currentSquare.y + 1 });
+        }
+        accountedForSquares.add(squareKey(currentSquare));
+      }
+      groups.push(groupSize);
+    }
+  }
+  return groups.length;
+};
 
 const EXAMPLE_INPUT = 'flqrgnkx';
 const PUZZLE_INPUT = 'hfdlxzhv';
@@ -58,3 +104,5 @@ simpleTest(usedSquares, EXAMPLE_INPUT, 8108);
 test('Part One answer', equalResult(usedSquares(PUZZLE_INPUT), 8230));
 
 console.log('Part Two');
+simpleTest(countRegions, EXAMPLE_INPUT, 1242);
+test('Part Two answer', equalResult(countRegions(PUZZLE_INPUT), 1103));
