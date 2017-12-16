99b1a4a5d4cf2dbc5cc30787e6e68444c23d5984
DallonF <dallon.feldner@gmail.com>
2017-12-15 20:11:01 -0600

    It works, but takes forever to run. Starting some performance optimization

--- day14.ts
+++ day14.ts
@@ -1,23 +1,7 @@
 import * as _ from 'lodash';
 import test, { simpleTest, equalResult } from './test';
+import { hexToBinary } from './util';
 import { hash2 } from './day10';
-
-const HALF_BYTE = _.range(3, -1, -1).map(i => Math.pow(2, i));
-const hexToBinary = (hex: string) =>
-  [...hex]
-    .map(halfByte => {
-      const value = parseInt(halfByte, 16);
-      let remainingValue = value;
-      return HALF_BYTE.map(place => {
-        if (remainingValue >= place) {
-          remainingValue -= place;
-          return '1';
-        } else {
-          return '0';
-        }
-      }).join('');
-    })
-    .join('');
 
 type Grid = boolean[][];
 
@@ -91,15 +75,6 @@ const EXAMPLE_INPUT = 'flqrgnkx';
 const PUZZLE_INPUT = 'hfdlxzhv';
 
 console.log('Part One');
-simpleTest(hexToBinary, 'f', '1111', 'hexToBinary');
-simpleTest(hexToBinary, '0', '0000', 'hexToBinary');
-simpleTest(hexToBinary, '3', '0011', 'hexToBinary');
-simpleTest(
-  hexToBinary,
-  'a0c2017',
-  '1010000011000010000000010111',
-  'hexToBinary'
-);
 simpleTest(usedSquares, EXAMPLE_INPUT, 8108);
 test('Part One answer', equalResult(usedSquares(PUZZLE_INPUT), 8230));
 
