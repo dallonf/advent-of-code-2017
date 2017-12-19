7b6c47994e829afb19dd53899c2eec6412c680c4
DallonF <dallon.feldner@gmail.com>
2017-12-18 18:45:20 -0600

    Day 18 Part One

--- day18.ts
+++ day18.ts
@@ -1,5 +1,5 @@
 import test, { simpleTest, equalResult } from './test';
-import { OS_EOL } from './util';
+import { OS_EOL, readLines } from './util';
 
 type Value = string | number;
 type SideEffect =
@@ -21,30 +21,80 @@ interface Instruction {
 
 const INSTRUCTION_FNS = {
   snd: ((input, registers) => ({
-    type: 'noop',
+    type: 'playSound',
+    frequency: getValue(input.x, registers),
   })) as InstructionFn,
-  set: ((input, registers) => ({
-    type: 'noop',
-  })) as InstructionFn,
-  add: ((input, registers) => ({
-    type: 'noop',
-  })) as InstructionFn,
-  mul: ((input, registers) => ({
-    type: 'noop',
-  })) as InstructionFn,
-  mod: ((input, registers) => ({
-    type: 'noop',
-  })) as InstructionFn,
-  rcv: ((input, registers) => ({
-    type: 'noop',
-  })) as InstructionFn,
-  jgz: ((input, registers) => ({
-    type: 'noop',
-  })) as InstructionFn,
+  set: ((input, registers) => {
+    if (typeof input.x !== 'string')
+      throw new Error(
+        'Expected X of set instruction to be register, got ' + input.x
+      );
+    if (input.y == null)
+      throw new Error('Set instruction called without Y value');
+    return {
+      type: 'set',
+      register: input.x,
+      value: getValue(input.y, registers),
+    };
+  }) as InstructionFn,
+  add: ((input, registers) => {
+    if (typeof input.x !== 'string')
+      throw new Error(
+        'Expected X of add instruction to be register, got ' + input.x
+      );
+    if (input.y == null)
+      throw new Error('Set instruction called without Y value');
+    return {
+      type: 'set',
+      register: input.x,
+      value: getValue(input.x, registers) + getValue(input.y, registers),
+    };
+  }) as InstructionFn,
+  mul: ((input, registers) => {
+    if (typeof input.x !== 'string')
+      throw new Error(
+        'Expected X of mul instruction to be register, got ' + input.x
+      );
+    if (input.y == null)
+      throw new Error('Mul instruction called without Y value');
+    return {
+      type: 'set',
+      register: input.x,
+      value: getValue(input.x, registers) * getValue(input.y, registers),
+    };
+  }) as InstructionFn,
+  mod: ((input, registers) => {
+    if (typeof input.x !== 'string')
+      throw new Error(
+        'Expected X of mod instruction to be register, got ' + input.x
+      );
+    if (input.y == null)
+      throw new Error('Mod instruction called without Y value');
+    return {
+      type: 'set',
+      register: input.x,
+      value: getValue(input.x, registers) % getValue(input.y, registers),
+    };
+  }) as InstructionFn,
+  rcv: ((input, registers) =>
+    getValue(input.x, registers) !== 0
+      ? { type: 'recover' }
+      : { type: 'noop' }) as InstructionFn,
+  jgz: ((input, registers) => {
+    if (input.y == null)
+      throw new Error('Jgz instruction called without Y value');
+    if (getValue(input.x, registers) > 0) {
+      // return { type: 'noop', by: getValue(input.y, registers) };
+      return { type: 'jump', by: getValue(input.y, registers) };
+    } else {
+      return { type: 'noop' };
+    }
+  }) as InstructionFn,
 };
 
 const getValue = (input: Value, registers: Map<string, number>): number => {
-  return 0;
+  if (typeof input === 'number') return input;
+  return registers.get(input) || 0;
 };
 
 const parseInstruction = (input: string): Instruction => {
@@ -56,12 +106,40 @@ const parseInstruction = (input: string): Instruction => {
 
   let x: Value = tokens[1];
   if (!x) throw new Error(`Missing x token in command "${input}"`);
-  if (x.match(/^[0-9]+$/)) x = parseInt(x, 10);
+  if (x.match(/^-?[0-9]+$/)) x = parseInt(x, 10);
 
   let y: Value | undefined = tokens[2] || undefined;
-  if (y && y.match(/^[0-9]+$/)) y = parseInt(y, 10);
+  if (y && y.match(/^-?[0-9]+$/)) y = parseInt(y, 10);
 
   return { type: type as InstructionType, x, y };
+};
+
+const executeInstructions = (instructions: Instruction[]): number => {
+  const registers = new Map<string, number>();
+  let lastSoundPlayed: number | null = null;
+  for (let i = 0; i < instructions.length; i++) {
+    const currentInstruction = instructions[i];
+    const fn = INSTRUCTION_FNS[currentInstruction.type];
+    const effect = fn(currentInstruction, registers);
+
+    switch (effect.type) {
+      case 'jump':
+        // The -1 offsets the i++ that happens after the loop.
+        // dirty, but don't feel like fixing it
+        i += effect.by - 1;
+        break;
+      case 'playSound':
+        lastSoundPlayed = effect.frequency;
+        break;
+      case 'recover':
+        if (!lastSoundPlayed) throw new Error('Nothing to recover');
+        return lastSoundPlayed;
+      case 'set':
+        registers.set(effect.register, effect.value);
+        break;
+    }
+  }
+  throw new Error('Reached end of instructions without recovering sound');
 };
 
 const EXAMPLE_INPUT = `
@@ -80,6 +158,8 @@ jgz a -2
   .filter(x => x)
   .map(parseInstruction);
 
+const PUZZLE_INPUT = readLines('./day18input.txt').map(parseInstruction);
+
 console.log('Part One');
 test(
   'parse input',
@@ -95,3 +175,12 @@ test(
     { deepEqual: true }
   )
 );
+simpleTest(
+  parseInstruction,
+  'jgz a -1',
+  { type: 'jgz', x: 'a', y: -1 },
+  'parseInstruction handles negative input',
+  { deepEqual: true }
+);
+test('execute example', equalResult(executeInstructions(EXAMPLE_INPUT), 4));
+test('Part One answer', equalResult(executeInstructions(PUZZLE_INPUT), 2951));
