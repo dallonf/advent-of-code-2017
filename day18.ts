4f9715a3547bc08ee40e0a1f520d80f4310ec54d
DallonF <dallon.feldner@gmail.com>
2017-12-18 19:24:24 -0600

    Day 18 Part Two

--- day18.ts
+++ day18.ts
@@ -4,7 +4,9 @@ import { OS_EOL, readLines } from './util';
 type Value = string | number;
 type SideEffect =
   | { type: 'playSound'; frequency: number }
+  | { type: 'send'; value: number }
   | { type: 'recover' }
+  | { type: 'receive'; register: string }
   | { type: 'jump'; by: number }
   | { type: 'set'; register: string; value: number }
   | { type: 'noop' };
@@ -92,6 +94,21 @@ const INSTRUCTION_FNS = {
   }) as InstructionFn,
 };
 
+const INSTRUCTION_FUNCTIONS_PART_TWO: typeof INSTRUCTION_FNS = {
+  ...INSTRUCTION_FNS,
+  snd: (input, registers) => ({
+    type: 'send',
+    value: getValue(input.x, registers),
+  }),
+  rcv: (input, registers) => {
+    if (typeof input.x !== 'string')
+      throw new Error(
+        'Expected X of rcv instruction to be register, got ' + input.x
+      );
+    return { type: 'receive', register: input.x };
+  },
+};
+
 const getValue = (input: Value, registers: Map<string, number>): number => {
   if (typeof input === 'number') return input;
   return registers.get(input) || 0;
@@ -137,9 +154,94 @@ const executeInstructions = (instructions: Instruction[]): number => {
       case 'set':
         registers.set(effect.register, effect.value);
         break;
+      case 'noop':
+        break;
+      default:
+        throw new Error(`Unrecognized side effect ${effect.type}`);
     }
   }
   throw new Error('Reached end of instructions without recovering sound');
+};
+
+function* executeDuetProgram(instructions: Instruction[], programId: number) {
+  const registers = new Map<string, number>();
+  registers.set('p', programId);
+  for (let i = 0; i < instructions.length; i++) {
+    const currentInstruction = instructions[i];
+    const fn = INSTRUCTION_FUNCTIONS_PART_TWO[currentInstruction.type];
+    const effect = fn(currentInstruction, registers);
+
+    switch (effect.type) {
+      case 'jump':
+        // The -1 offsets the i++ that happens after the loop.
+        // dirty, but don't feel like fixing it
+        i += effect.by - 1;
+        break;
+      case 'set':
+        registers.set(effect.register, effect.value);
+        break;
+      case 'send':
+        yield effect.value;
+        break;
+      case 'receive':
+        const message: number = yield;
+        registers.set(effect.register, message);
+      case 'noop':
+        break;
+      default:
+        throw new Error(`Unrecognized side effect ${effect.type}`);
+    }
+  }
+  throw new Error('Reached end of instructions without reaching deadlock');
+}
+
+// Generators are cool and all, but I'm not really happy with this solution.
+// Maybe later I'll find a fully functional approach.
+const executeDuet = (instructions: Instruction[]): number => {
+  const program0 = executeDuetProgram(instructions, 0);
+  const program0Queue: number[] = [];
+  let program0Waiting = false;
+  const program1 = executeDuetProgram(instructions, 1);
+  const program1Queue: number[] = [];
+  let program1Waiting = false;
+
+  let program1ValuesSent = 0;
+
+  while (true) {
+    const program0Yield: IteratorResult<number> | null = program0Waiting
+      ? program0Queue.length ? program0.next(program0Queue.shift()) : null
+      : program0.next();
+    if (!program0Yield) {
+      /* no-op */
+    } else if (program0Yield.value != null) {
+      program0Waiting = false;
+      program1Queue.push(program0Yield.value);
+    } else {
+      program0Waiting = true;
+    }
+    const program1Yield: IteratorResult<number> | null = program1Waiting
+      ? program1Queue.length ? program1.next(program1Queue.shift()) : null
+      : program1.next();
+    if (!program1Yield) {
+      /* no-op */
+    } else if (program1Yield.value != null) {
+      program1Waiting = false;
+      program1ValuesSent += 1;
+      program0Queue.push(program1Yield.value);
+    } else {
+      program1Waiting = true;
+    }
+
+    if (
+      program0Waiting &&
+      program1Waiting &&
+      !program0Queue.length &&
+      !program1Queue.length
+    ) {
+      // If both programs are waiting and have no values in their queue, it's a deadlock
+      return program1ValuesSent;
+    }
+  }
 };
 
 const EXAMPLE_INPUT = `
@@ -184,3 +286,20 @@ simpleTest(
 );
 test('execute example', equalResult(executeInstructions(EXAMPLE_INPUT), 4));
 test('Part One answer', equalResult(executeInstructions(PUZZLE_INPUT), 2951));
+
+console.log('Part Two');
+const PART_TWO_EXAMPLE_INPUT = `
+snd 1
+snd 2
+snd p
+rcv a
+rcv b
+rcv c
+rcv d
+`
+  .split(OS_EOL)
+  .filter(x => x)
+  .map(parseInstruction);
+
+test('execute example', equalResult(executeDuet(PART_TWO_EXAMPLE_INPUT), 3));
+test('Part Two answer', equalResult(executeDuet(PUZZLE_INPUT), 7366));
