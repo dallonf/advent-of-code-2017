80750eacee6ccb05427f075e6ab59635cf7e849b
DallonF <dallon.feldner@gmail.com>
2017-12-21 08:25:49 -0600

    Day 21 Part One

--- day21.ts
+++ day21.ts
@@ -1,5 +1,6 @@
+import * as _ from 'lodash';
 import test, { equalResult } from './test';
-import { OS_EOL } from './util';
+import { OS_EOL, readLines } from './util';
 
 type Grid = boolean[][];
 interface Rule {
@@ -53,6 +54,73 @@ const parseRule = (input: string): Rule => {
 const patternMatches = (input: string, rule: Rule): boolean =>
   rule.matchPatterns.has(input);
 
+const pixelsOnAfterIterations = (rules: Rule[], iterations: number): number => {
+  let currentState = patternToGrid(STARTING_PATTERN);
+  for (let index = 0; index < iterations; index++) {
+    currentState = gridIteration(currentState, rules);
+  }
+  return currentState
+    .map(row => row.map(pixel => Number(pixel ? 1 : 0)).reduce((a, b) => a + b))
+    .reduce((a, b) => a + b);
+};
+
+const gridIteration = (gridState: Grid, rules: Rule[]): Grid => {
+  const size = gridState[0].length;
+  let numSquares: number, squareSize: number;
+  if (size % 2 === 0) {
+    numSquares = size / 2;
+    squareSize = 2;
+  } else if (size % 3 == 0) {
+    numSquares = size / 3;
+    squareSize = 3;
+  } else {
+    throw new Error(`Grid size ${size} not divisible by 2 or 3`);
+  }
+
+  const sourceSquares = _.flatten(
+    _.range(numSquares).map(x =>
+      _.range(numSquares).map(y => {
+        const offsetX = x * squareSize;
+        const offsetY = y * squareSize;
+        return {
+          x,
+          y,
+          square: _.range(squareSize).map(squareX =>
+            _.range(squareSize).map(squareY =>
+              getValue(gridState, offsetX + squareX, offsetY + squareY)
+            )
+          ) as Grid,
+        };
+      })
+    )
+  );
+
+  const outputSquares = sourceSquares.map(square => {
+    const pattern = gridToPattern(square.square);
+    const matchingRule = rules.find(rule => patternMatches(pattern, rule));
+    if (!matchingRule) {
+      throw new Error(`No matching rule for grid square "${pattern}"`);
+    }
+    return { ...square, square: patternToGrid(matchingRule.output) };
+  });
+
+  const newSquareSize = squareSize + 1;
+  const newGrid = _.range(numSquares * newSquareSize).map(squareX =>
+    _.range(numSquares * newSquareSize).map(() => false)
+  );
+  outputSquares.forEach(square => {
+    const offsetX = square.x * newSquareSize;
+    const offsetY = square.y * newSquareSize;
+    square.square.forEach((row, squareX) =>
+      row.forEach((value, squareY) => {
+        newGrid[offsetY + squareY][offsetX + squareX] = value;
+      })
+    );
+  });
+
+  return newGrid;
+};
+
 console.log('Part One');
 const EXAMPLE_INPUT = `
 ../.# => ##./#../...
@@ -61,8 +129,9 @@ const EXAMPLE_INPUT = `
   .split(OS_EOL)
   .filter(x => x);
 
+const PUZZLE_INPUT = readLines('./day21input.txt');
+
 const sampleRule = parseRule('.#./..#/### => ..../..../..../....');
-console.log(sampleRule);
 test(
   'flipped match',
   equalResult(patternMatches('.#./..#/###', sampleRule), true)
@@ -82,4 +151,17 @@ test(
 test(
   'flipped non-match',
   equalResult(patternMatches('#.#/..#/.#.', sampleRule), false)
+);
+
+const exampleRules = EXAMPLE_INPUT.map(parseRule);
+const puzzleRules = PUZZLE_INPUT.map(parseRule);
+
+test(
+  'pixelsOnAfterIterations',
+  equalResult(pixelsOnAfterIterations(exampleRules, 2), 12)
+);
+
+test(
+  'Part One answer',
+  equalResult(pixelsOnAfterIterations(puzzleRules, 5), 190)
 );
