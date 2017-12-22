c257d17e24a4773c51f101bcef55dad0a567e9f8
DallonF <dallon.feldner@gmail.com>
2017-12-22 08:08:21 -0600

    Day 22 Part One

--- day22.ts
+++ day22.ts
@@ -1,9 +1,22 @@
 import test, { simpleTest, equalResult } from './test';
-import { OS_EOL } from './util';
+import * as _ from 'lodash';
+import { OS_EOL, readLines } from './util';
 
 // This one is kind of sloppy because I didn't have a lot of time
 
-type Grid = Map<number, Map<number, boolean>>;
+type Grid = {
+  minX: number;
+  maxX: number;
+  minY: number;
+  maxY: number;
+  map: Map<number, Map<number, boolean>>;
+};
+
+type Carrier = {
+  currentX: number;
+  currentY: number;
+  direction: Direction;
+};
 
 enum Direction {
   UP = 0,
@@ -13,18 +26,26 @@ enum Direction {
 }
 
 const checkNode = (grid: Grid, x: number, y: number) => {
-  const column = grid.get(x);
+  const column = grid.map.get(x);
   if (!column) return false;
   return column.get(y) || false;
 };
 
+/**
+ * @param grid WARNING: will mutate the grid
+ */
 const writeNode = (grid: Grid, x: number, y: number, value: boolean) => {
-  let column = grid.get(x);
+  const { map } = grid;
+  let column = map.get(x);
   if (!column) {
     column = new Map<number, boolean>();
-    grid.set(x, column);
+    map.set(x, column);
   }
   column.set(y, value);
+  if (x > grid.maxX) grid.maxX = x;
+  if (x < grid.minX) grid.minX = x;
+  if (y > grid.maxY) grid.maxY = y;
+  if (y < grid.minY) grid.minY = y;
 };
 
 const parseInput = (input: string[]) => {
@@ -32,13 +53,32 @@ const parseInput = (input: string[]) => {
   const xSize = input[0].length;
   const originOffsetX = Math.floor(xSize / 2);
   const originOffsetY = Math.floor(ySize / 2);
-  const grid = new Map<number, Map<number, boolean>>();
+  const grid = {
+    minX: 0,
+    maxX: 0,
+    minY: 0,
+    maxY: 0,
+    map: new Map<number, Map<number, boolean>>(),
+  };
   input.forEach((row, y) =>
     [...row].forEach((val, x) => {
       writeNode(grid, x - originOffsetX, y - originOffsetY, val === '#');
     })
   );
   return grid;
+};
+
+const debugGrid = (grid: Grid, carrier?: Carrier) => {
+  console.log('---- grid ----');
+  for (let y = grid.minY; y <= grid.maxY; y++) {
+    console.log(
+      _.range(grid.minX, grid.maxX + 1)
+        .map(x => (checkNode(grid, x, y) ? '#' : '.'))
+        .join(' ')
+    );
+  }
+  if (carrier)
+    console.log({ ...carrier, direction: Direction[carrier.direction] });
 };
 
 const turnRight = (direction: Direction) => {
@@ -83,10 +123,7 @@ const move = (x: number, y: number, direction: Direction) => {
 /**
  * @param grid WARNING: will mutate the grid
  */
-const simulateStep = (
-  grid: Grid,
-  carrier: { currentX: number; currentY: number; direction: Direction }
-) => {
+const simulateStep = (grid: Grid, carrier: Carrier) => {
   const isCurrentNodeInfected = checkNode(
     grid,
     carrier.currentX,
@@ -100,11 +137,17 @@ const simulateStep = (
   const { x: newX, y: newY } = move(
     carrier.currentX,
     carrier.currentY,
-    carrier.direction
+    newDirection
   );
 
+  const newCarrier = {
+    currentX: newX,
+    currentY: newY,
+    direction: newDirection,
+  };
+
   return {
-    carrier: { currentX: newX, currentY: newY, direction: newDirection },
+    carrier: newCarrier,
     didInfect: !isCurrentNodeInfected,
   };
 };
@@ -131,6 +174,8 @@ const EXAMPLE_INPUT = `
   .split(OS_EOL)
   .filter(x => x);
 
+const PUZZLE_INPUT = readLines('./day22input.txt');
+
 console.log('Part One');
 const exampleGrid = parseInput(EXAMPLE_INPUT);
 test('-1,0 is infected', checkNode(exampleGrid, -1, 0));
@@ -150,5 +195,13 @@ test(
   equalResult(
     countInfectionsAfterIterations(parseInput(EXAMPLE_INPUT), 10000),
     5587
+  )
+);
+
+test(
+  'Part One answer',
+  equalResult(
+    countInfectionsAfterIterations(parseInput(PUZZLE_INPUT), 10000),
+    0
   )
 );
